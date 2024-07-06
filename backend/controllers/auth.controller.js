8d92b7be14da40cbf1e02f50ba798235dbf203d6
riaz9191 <riazahammed9191@gmail.com>
2024-07-06 10:03:41 +0600

    auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,42 @@
+const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  const { first_name, last_name, email, phone, password, user_role, gender, day, month, year } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const dob = new Date(`${year}-${month}-${day}`);
+    const user = new User({ first_name, last_name, email, phone, password, user_role, gender, dob });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+      const token = jwt.sign({ userId: user._id }, 'secret', { expiresIn: '1h' });
+      res.json({ token });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { register, login };
